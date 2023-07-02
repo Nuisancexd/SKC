f8a076bf04a09015af51c60dfd2f179b4aea7179
Nuisancexd <76814800+Nuisancexd@users.noreply.github.com>
2023-07-02 22:08:35 +0500

    Update encrypt.h

--- src/cryptography/encrypt.h
+++ src/cryptography/encrypt.h
@@ -2,22 +2,36 @@
 
 #include "stack.h"
 
-#include "type.h"
-
 #include "BijectiveMappingConst.h"
 
-extern void hash_state(Stack* stack);
 
-extern void dehash_state(Stack* stack);
+typedef uint32_t internal;
+
+typedef struct HashContext
+{
+    internal hash = 0;
+    internal buffer_hash;
+    internal iterator = 0;
+
+    internal j_index = 0;
+    internal i_index__ = 0;
+}GHC;
+
+
+extern void hash_state_of_cr(Stack* stack, GHC* ghc, size_t index);
+
+extern void hash_state_of_encr(Stack* stack, GHC* ghc, size_t index);
+
+extern void hash_xor_key(Stack* stack, GHC* ghc, size_t index);
+
+extern void merge_state_key_cr(Stack* stack, Stack* stack2, GHC* ghc, size_t index);
+
+extern void merge_state_key_encr(Stack* stack, Stack* stack2, GHC* ghc, size_t index);
+
+extern void hash_key(Stack* stack, GHC* ghc, size_t index);
+
+static void update__(GHC* ghc, Stack* stack_st, size_t index);
 
 extern int32_t gray_encode(int32_t a);
 
 extern int gray_dencode(int a);
-
-extern void Hash_xor_key(Stack* stack);
-
-extern void hash_key(Stack* stack);
-
-extern void dehash_key(Stack* stack);
-
-extern void merge_state_key(Stack* stack, Stack* stack2);
